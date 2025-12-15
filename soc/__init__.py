60edc4913ee1ac8ac5f96dcddcecb1656a6fcee3
Erik Donath <erikdonath@proton.me>
2025-12-15 17:48:26 +0100

    Fixed Import

--- soc/__init__.py
+++ soc/__init__.py
@@ -3,7 +3,7 @@ RISC-V SoC Package
 Modular SoC builder for FPGA targets
 """
 
-from .config import SoCConfig, FirmwareTarget
+from .config import SoCConfig
 from .base import BaseSoC
 from .builder import build_soc
 
